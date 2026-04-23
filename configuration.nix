f80a5c3dfb255a2d3fbccd58c36fcd59a6de4d2f
Julian <julianorlich1@gmail.com>
2026-04-23 15:26:02 +0200

    Quiet boot output; update AGENTS instructions

--- configuration.nix
+++ configuration.nix
@@ -28,9 +28,22 @@ in
     enable = true;
     efiSupport = true;
     device = "nodev";
+    timeout = 0;
   };
   boot.loader.efi.canTouchEfiVariables = true;
   boot.plymouth.enable = true;
+  boot.initrd.verbose = false;
+  boot.consoleLogLevel = 3;
+  boot.kernelParams = [
+    "quiet"
+    "splash"
+    "loglevel=3"
+    "udev.log_level=3"
+    "rd.udev.log_level=3"
+    "systemd.show_status=auto"
+    "vt.global_cursor_default=0"
+  ];
+  boot.initrd.kernelModules = [ "amdgpu" ];
   boot.extraModprobeConfig = ''
     options btusb enable_autosuspend=n
   '';
