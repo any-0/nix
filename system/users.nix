67664c09ba4087e5c3f6713e31594b4b75a06310
Julian <julianorlich1@gmail.com>
2026-07-30 12:38:27 +0200

    Update NixOS SSH authorization

--- system/users.nix
+++ system/users.nix
@@ -8,8 +8,7 @@
     extraGroups = [ "wheel" "networkmanager" "seat" "video" "input" "uinput" "docker" ];
     shell = pkgs.zsh;
     openssh.authorizedKeys.keys = [
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPD75TBVAP3IJF5Ky2aJxIUa8Ya/fcQV88ZKf9naKYly remote-control-deploy"
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOTYpjvYZ3UU/pHLlleyrE/XnaAvMb6fyK6jGEicfRr+ hermes@proxmox-vm-20260708"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPD75TBVAP3IJF5Ky2aJxIUa8Ya/fcQV88ZKf9naKYly pc"
     ];
   };
 }
